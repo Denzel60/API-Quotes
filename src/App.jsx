8184df2b5fc931f23eb4a6fc9b836719e9ac6389
Denzel <lawrencedenzel60@gmail.com>
2024-06-05 12:43:34 +0300

    Complete interface and still working on the state hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unescaped-entities */
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const todayQuote = async _ => {
+    const url = "https://zenquotes.io/api/today";
+    const response = await fetch(url);
+    console.log(response);
+    var data = await response.json();
+    return data;
+  }
+
+  const [quote, setQuote] = useState(`Quote of the day is: ${todayQuote}`);
+
+  const handleQuotes = async (e) => {
+
+    e.preventDefault();
+
+    try {
+
+      const response = await fetch("https://zenquotes.io/api/random");
+      console.log(response);
+      var data = await response.json();
+      console.log(data);
+      setQuote(data);
+
+    } catch (error) {
+      // console.log("There was an error")
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="quotes-container">
+        <h1><a href="https://github.com/Denzel60">By Denzel Lawrence</a></h1>
+        <button type="button" onClick={handleQuotes}>Generate Random Quote</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>{quote}</h2>
+      <h2>{setQuote}</h2>
     </>
   )
 }
