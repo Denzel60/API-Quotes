c450d74c5db691790da08c9f37d1790e582520a9
Denzel <lawrencedenzel60@gmail.com>
2024-06-05 14:30:15 +0300

    A functional API that logs quotes to the console from ninja API

--- src/App.jsx
+++ src/App.jsx
@@ -5,41 +5,50 @@ import './App.css'
 
 function App() {
 
-  const todayQuote = async _ => {
-    const url = "https://zenquotes.io/api/today";
-    const response = await fetch(url);
-    console.log(response);
-    var data = await response.json();
-    return data;
-  }
+  // const todayQuote = async () => {
+  //   const url = "https://zenquotes.io/api/today";
+  //   const response = await fetch(url);
+  //   console.log(response);
+  //   const data = await response.json();
+  //   return data;
+  // }
+  // let quoteOfTheDay = todayQuote();
 
-  const [quote, setQuote] = useState(`Quote of the day is: ${todayQuote}`);
+  const [quote, setQuote] = useState(null);
 
   const handleQuotes = async (e) => {
 
     e.preventDefault();
+    const apiKey = "5mF1kwUcslFYmOpmVHEV8w==uH3yGQoATwxosfAv";
 
     try {
+      const options = {
+        method: "GET",
+        headers: {
+          "X-Api-Key": apiKey,
+        },
+      };
+      const apiURL = 'https://api.api-ninjas.com/v1/quotes?category=happiness';
 
-      const response = await fetch("https://zenquotes.io/api/random");
-      console.log(response);
-      var data = await response.json();
-      console.log(data);
-      setQuote(data);
+      const response = await fetch(apiURL, options);
+      const data = await response.json();
+
+      console.log(data[0])
 
     } catch (error) {
-      // console.log("There was an error")
+      console.log("There was an error")
     }
   }
+
 
   return (
     <>
       <div className="quotes-container">
-        <h1><a href="https://github.com/Denzel60">By Denzel Lawrence</a></h1>
+        <h1>By: <a href="https://github.com/Denzel60">Denzel Lawrence</a></h1>
         <button type="button" onClick={handleQuotes}>Generate Random Quote</button>
       </div>
       <h2>{quote}</h2>
-      <h2>{setQuote}</h2>
+      <h2>Random Quote: {setQuote}</h2>
     </>
   )
 }
